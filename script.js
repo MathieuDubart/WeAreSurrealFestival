4bf9ef1b1cc0f9c9d52d68c6df989929409fd42e
Mathieu <01.basque_grizzlis@icloud.com>
2021-12-14 17:43:21 +0100

    third commit

--- script.js
+++ script.js
@@ -1,8 +1,103 @@
-console.log(window.location.href)
+let typoSelector = document.querySelector("#typoContainer")
 
-let typoSelector = document.querySelector("#typoContainer p")
+let xhttp = new XMLHttpRequest();
+let header= document.getElementsByTagName("header");
 
-const info = require('bapt/baptiste.json');
-console.log(JSON.parse(info)) // should output valid data
+// ------------------ Get URL Parameters ------------------//
 
-typoSelector.innerHTML(info.textForExample)
+let str = window.location.href;
+let url = new URL(str);
+let search_params = new URLSearchParams(url.search); 
+if(search_params.has('version')) {
+  let version = search_params.get('version');
+  if (version === "1") {
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let response_V1=this.responseText;
+            let baptisteInfos = JSON.parse(response_V1);
+            header.style.backgroundImage = baptisteInfos.backgroundImage
+        }
+    };
+    
+    xhttp.open("GET", "version_1/baptiste.json", true);
+    xhttp.send();
+  }
+  if (version === "2") {
+      xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let response_V2=this.responseText;
+        let killianInfos = JSON.parse(response_V2);
+        header.style.backgroundImage = killianInfos.backgroundImage
+    }
+    };
+
+    xhttp.open("GET", "version_2/killian.json", true);
+    xhttp.send();
+  }
+  if (version === "3") {
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let response_V3=this.responseText;
+            let melisseInfos = JSON.parse(response_V3);
+            header.style.backgroundImage = melisseInfos.backgroundImage
+        }
+    };
+    
+    xhttp.open("GET", "version_3/melisse.json", true);
+    xhttp.send();
+  }
+}
+
+
+// ------------------ Swiper for concerts ------------------//
+
+const swiper = new Swiper('.swiper', {
+// Optional parameters
+direction: 'horizontal',
+loop: true,
+
+pagination: {
+    el: '.swiper-pagination',
+},
+
+// Navigation arrows
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+},
+
+autoplay: {
+    delay: 4000,
+    },
+
+});
+
+// ------------------ Countdown -------------------//
+
+let countDownDate = new Date("Mar 18, 2022 18:00:00").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+  // Get today's date and time
+  let now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "j. " + hours + "h. "
+  + minutes + "m. " + seconds + "s. ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Le festival a commencé";
+  }
+}, 1000);
